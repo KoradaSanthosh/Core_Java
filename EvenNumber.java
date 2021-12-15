630cd79d94eba6806628712d0a25eae1ba41dea5
KoradaSanthosh <96012023+KoradaSanthosh@users.noreply.github.com>
2021-12-15 08:55:20 +0530

    Add files via upload

---
+++ EvenNumber.java
@@ -0,0 +1,15 @@
+package com;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10;
+		if(x%2 == 0)
+			System.out.println("Given number is even");
+		else
+			System.out.println("Given is odd");
+
+	}
+
+}
---
+++ SumoffTwoNumbers.java
@@ -0,0 +1,19 @@
+package com;
+
+public class SumoffTwoNumbers {
+
+	public static void main(String[] args) {
+		// Addition of two numbers
+		int num=5;
+		int numb=25;
+		int sum=num+numb;
+		System.out.println("sum of 5 and 25:" + sum);
+
+		//Another way
+		int number=12,number_a=10,add;
+		add=number+number_a;
+		System.out.println(add);
+		
+	}
+
+}
