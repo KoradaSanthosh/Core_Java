15490a32849564d165bc81701f9be9d90f257031
KoradaSanthosh <96012023+KoradaSanthosh@users.noreply.github.com>
2022-01-18 21:11:40 +0530

    Add files via upload

---
+++ DepartmentRepository.java
@@ -0,0 +1,10 @@
+package org.postgresql.Repository;
+
+import org.postgresql.Model.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+}
